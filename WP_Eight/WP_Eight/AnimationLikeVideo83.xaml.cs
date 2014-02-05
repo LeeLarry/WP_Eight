b5030774c7f44135a6036ca55f85c3c699b3d97b
larry <576002177@qq.com>
2014-02-05 23:32:41 +0800

    第八章剩余练习

---
+++ WP_Eight/WP_Eight/AnimationLikeVideo83.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+
+namespace WP_Eight
+{
+    public partial class AnimationLikeVideo83 : PhoneApplicationPage
+    {
+        TimeSpan startTime;
+
+        public AnimationLikeVideo83()
+        {
+            InitializeComponent();
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+            /*表示您的应用程序的显示图面。
+             * CompositionTarget 是一个类，表示正在其上绘制您的应用程序的显示图面。 
+             * WPF 动画引擎为创建基于帧的动画提供了许多功能。 但是，在有些应用程序方案中，
+             * 您需要根据每个帧控制呈现。 使用 CompositionTarget 对象，可以基于每个帧回调来创建自定义动画。
+             */
+        }
+
+        void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            TimeSpan renderingTime = (e as RenderingEventArgs).RenderingTime;
+
+            if (startTime.Ticks == 0)
+            {
+                startTime = renderingTime;
+            }
+            else
+            {
+                TimeSpan elapseTime = renderingTime - startTime;
+                rotate.Angle = 180 * elapseTime.TotalSeconds % 360;
+            }
+        }
+    }
+}
---
+++ WP_Eight/WP_Eight/ManipulationEventDemo84.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace WP_Eight
+{
+    public partial class ManipulationEventDemo84 : PhoneApplicationPage
+    {
+        public ManipulationEventDemo84()
+        {
+            InitializeComponent();
+        }
+
+        private void Ellipse_ManipulationDelta(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
+        {
+            Ellipse ellipse = sender as Ellipse;
+            CompositeTransform xform = ellipse.RenderTransform as CompositeTransform;
+            if (e.DeltaManipulation.Scale.X > 0 || e.DeltaManipulation.Scale.Y > 0)
+            {
+                double maxScale = Math.Max(e.DeltaManipulation.Scale.X, e.DeltaManipulation.Scale.Y);
+                xform.ScaleX *= maxScale;
+                xform.ScaleY *= maxScale;
+            }//这是有两个手指在屏幕上时,scale通常不为零,也就是在进行缩放
+
+            xform.TranslateX += e.DeltaManipulation.Translation.X;
+            xform.TranslateY += e.DeltaManipulation.Translation.Y;
+            //这是只有一个手指时,只进行平移操作
+            e.Handled = true;
+        }
+    }
+}
